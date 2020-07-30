480ec174102a3ef4735627d042a47903e9e66243
prydin <prydin@vmware.com>
2020-07-30 13:33:55 -0400

    Added distance-based pricing calculations

--- pricing/src/main/java/vexpress/pricing/PricingApplication.java
+++ pricing/src/main/java/vexpress/pricing/PricingApplication.java
@@ -2,10 +2,13 @@ package vexpress.pricing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(Config.class)
 public class PricingApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(PricingApplication.class, args);
-	}
+
+  public static void main(final String[] args) {
+    SpringApplication.run(PricingApplication.class, args);
+  }
 }
--- pricing/src/main/java/vexpress/pricing/PricingController.java
+++ pricing/src/main/java/vexpress/pricing/PricingController.java
@@ -1,16 +1,30 @@
 package vexpress.pricing;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class PricingController {
+  @Autowired private Config config;
+
   @GetMapping("/price")
   public double getPrice(
       @RequestParam final String fromZip,
       @RequestParam final String toZip,
       @RequestParam final double weight) {
-    return weight * Math.PI;
+
+    final RestTemplate restTemplate = new RestTemplate();
+    final UriComponentsBuilder builder =
+        UriComponentsBuilder.fromHttpUrl(config.getZipcodeUrl() + "/distance")
+            .queryParam("fromZip", fromZip)
+            .queryParam("toZip", toZip);
+    final ResponseEntity<DistanceResponse> response =
+        restTemplate.getForEntity(builder.build().toUri(), DistanceResponse.class);
+    return weight * Math.PI + 0.02 * response.getBody().getDistance();
   }
 }
