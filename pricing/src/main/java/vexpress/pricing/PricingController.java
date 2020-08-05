8e983fb0968fa688beb8c376a116b58a93ac6ab2
prydin <prydin@vmware.com>
2020-08-05 12:18:53 -0400

    Added orders and scheduling

--- pricing/src/main/java/vexpress/pricing/PricingController.java
+++ pricing/src/main/java/vexpress/pricing/PricingController.java
@@ -3,6 +3,7 @@ package vexpress.pricing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -17,8 +18,8 @@ public class PricingController {
     return "OK";
   }
 
-  @GetMapping("/price")
-  public double getPrice(
+  @PostMapping(value = "/price", consumes = "application/json", produces = "application/json")
+  public double getPrice(s
       @RequestParam final String fromZip,
       @RequestParam final String toZip,
       @RequestParam final double weight) {
